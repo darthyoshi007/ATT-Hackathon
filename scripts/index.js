f22863a3c44e99320336301e096ad35c62216325
Parth Doshi <doshi.parth9@gmail.com>
2016-07-15 19:32:59 -0700

    initial commit
    set everything up to use watson and firebase

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+var watson = require('watson-developer-cloud');
+
+var alchemy_language = watson.alchemy_language({
+  api_key: '9908391a4c1a215d4439bed414a8597a867632da'
+});
+
+var params = {
+  text: 'The quick brown fox jumped over the lazy panda.'
+};
+
+alchemy_language.extract(params, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
