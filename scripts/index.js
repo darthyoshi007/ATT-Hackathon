e8f601cd9906b78edd34a26f7e4118ac8f93f37b
Jakob Kuki <jakob@identify3d.com>
2016-07-15 20:43:39 -0700

    watson_image.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,9 @@
 var watson = require('watson-developer-cloud');
 var chokidar = require('chokidar');
 var mv = require('mv');
-
-var watcher = chokidar.watch('/Users/doshi/Downloads', {
+require('./watson_image.js')();
+log();
+var watcher = chokidar.watch('/Users/jakob/Downloads', {
   ignored: /[\/\\]\./,
   persistent: true,
   depth: 0
@@ -11,6 +12,7 @@ var watcher = chokidar.watch('/Users/doshi/Downloads', {
 var alchemy_language = watson.alchemy_language({
   api_key: '9908391a4c1a215d4439bed414a8597a867632da'
 });
+
 
 var params = {
   text: 'The quick brown fox jumped over the lazy panda. Vanguard is a good bank.'
@@ -23,13 +25,14 @@ alchemy_language.combined(params, function (err, response) {
     console.log(JSON.stringify(response, null, 2));
 });
 
+
 watcher.on('all', (event, path) => {
   console.log("Event: " + event + " | Path: " + path);
   var arr = path.split('.');
   if (event == "add" && arr[arr.length - 1] == "pdf"){
     console.log(path + " is a pdf. Moving to Documents Folder");
     var movePath = path.split('\\');
-    mv(path, '/Users/doshi/Documents/' + movePath[movePath.length - 1], {mkdirp: true}, function(err) {
+    mv(path, '/Users/jakob/Documents/' + movePath[movePath.length - 1], {mkdirp: true}, function(err) {
       if (err){
         console.log(err);
       }
